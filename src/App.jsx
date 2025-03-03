23521d5d3488d26013f96e266c3cc8e4c979e39a
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-03-03 22:31:37 +0000

    renderización inicial

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,14 @@
-import './App.css';
+import { useState } from "react";
+import LoginForm from "./components/login";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
-    </div>
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  return isAuthenticated ? (
+    <Dashboard setIsAuthenticated={setIsAuthenticated} />
+  ) : (
+    <LoginForm setIsAuthenticated={setIsAuthenticated} />
   );
 }
 
---
+++ src/components/login.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const LoginForm = () => {
+  return <h1>Holaee</h1>;
+};
+
+export default LoginForm;
