f47ef721bf9feb8cb6468b3b2483fada87078169
Julian David Ortega Solarte <95juliandos@gmail.com>
2025-03-03 22:52:44 +0000

    instalación sweatalert2

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,23 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import LoginForm from "./components/login";
 import Dashboard from "./pages/Dashboard";
 
 function App() {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(
+    () => JSON.parse(localStorage.getItem("isAuthenticated")) || false
+  );
+
+  document.body.style.background = isAuthenticated
+      ? "linear-gradient(135deg, rgb(124, 208, 238), rgb(5, 105, 196))"
+      : "linear-gradient(135deg, black, purple, gray)";
+
+  useEffect(() => {
+    if(isAuthenticated){
+      console.log("Autenticado");
+    }else{
+      console.log("No autenticado");
+    }
+  },[isAuthenticated]);
 
   return isAuthenticated ? (
     <Dashboard setIsAuthenticated={setIsAuthenticated} />
