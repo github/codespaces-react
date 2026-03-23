cff04c06bcc50581036f5a872ddbfc7f287f96dc
Nandinii1405 <101634633+Nandinii1405@users.noreply.github.com>
2026-03-23 19:26:45 +0530

    Update App.jsx
    removed word for pulling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces and git making changes pulling code<span className="heart">♥️</span> React
+          GitHub Codespaces and git making changes<span className="heart">♥️</span> React
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
