9f6f694f7baa6d0583fe9721b3a2620d2b829aa4
Nandinii1405 <tallamnandini@gmail.com>
2026-03-23 19:06:38 +0530

    1st change added a word

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
       <header className="App-header">
         <img src="Octocat.png" className="App-logo" alt="logo" />
         <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
+          GitHub Codespaces and git <span className="heart">♥️</span> React
         </p>
         <p className="small">
           Edit <code>src/App.jsx</code> and save to reload.
